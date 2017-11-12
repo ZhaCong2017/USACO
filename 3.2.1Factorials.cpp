f55121d868d4575a2ce67fea6941e4db0649f7e0
ZhaCong <32691184+ZhaCong2017@users.noreply.github.com>
2017-11-12 20:46:18 +0800

    Update 3.2.1Factorials.cpp

--- 3.2.1Factorials.cpp
+++ 3.2.1Factorials.cpp
@@ -8,13 +8,14 @@ LANG:C++
 #include<fstream>
 using namespace std;
 #define MAX 1000000000
+
 int main()
 {
 	ifstream fin("fact4.in");
 	ofstream fout("fact4.out");
 	int n;
 	fin >> n;
-	long long factorial = 1, result = 0;
+	long long factorial = 1;
 	for (int i = 1; i <= n; i++)
 	{
 		factorial *= i;
@@ -24,16 +25,7 @@ int main()
 			factorial %= MAX;
 	}
 
-	while (factorial)
-	{
-		if (factorial % 10 != 0)
-		{
-			result = factorial % 10;
-			break;
-		}
-		factorial %= 10;
-	}
-	fout << result << endl;
+	fout << factorial % 10 << endl;
 	//system("pasue");
 	return 0;
 }
